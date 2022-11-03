fe03749d85710a2954df18093ba3ad5f6b27f281
Leonardo Camargo <leonardo_camargo31@hotmail.com>
2022-11-03 17:07:04 -0300

    chore: add testing-library

--- src/sayHello.test.ts
+++ src/sayHello.test.ts
@@ -1,14 +1,17 @@
 import sayHello from "./sayHello";
-import {render} from "lit-html";
-
-beforeEach(() => {
-  render('', document.body);
-})
+import { render } from "lit-html";
+import { getByRole } from '@testing-library/dom'
 
 describe('sayHello', () => {
   it('says hello', () => {
     render(sayHello('world'), document.body);
     const component = document.querySelector('h1');
     expect(component).toBeDefined();
+
+    const  heading= getByRole(document.body, 'heading', {
+      level:1,
+      name: /Hello world/i 
+    })
+    expect(heading).toBeDefined()
   })
 })
--- src/sayHello.ts
+++ src/sayHello.ts
@@ -1,4 +1,5 @@
-import {html, TemplateResult} from 'lit-html';
+import { html, TemplateResult } from 'lit-html'
+import { until } from 'lit-html/directives/until'
 
 export default function sayHello(name: string): TemplateResult {
   return html`<h1>Hello ${name}</h1>`;
